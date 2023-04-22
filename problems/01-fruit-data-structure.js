623a0462d88e48744b5e6662d93d8460d17166ab
root <root@DESKTOP-ELPBQ96>
2023-04-22 18:26:16 -0400

    prob-4

--- problems/01-fruit-data-structure.js
+++ problems/01-fruit-data-structure.js
@@ -3,7 +3,7 @@ const fruits = require('../fruit-data')
 
 /* 01. `firstFruitObject()` - Return the first object in the fruits array
 
-console.log(firstFruitObject(fruits));
+
 // { genus: 'Malus', name: 'Apple', id: 6, family: 'Rosaceae', order:
 // 'Rosales', nutritions: { carbohydrates: 11.4, protein: 0.3, fat: 0.4,
 // calories: 52, sugar: 10.3 } }
@@ -11,13 +11,14 @@ console.log(firstFruitObject(fruits));
 
 function firstFruitObject(fruits) {
     // Your code here
+    return fruits[0];
 }
 
-
+//console.log(firstFruitObject(fruits));
 
 /* 02. `lastFruitObject()` - Return the last object in the fruits array
 
-console.log(lastFruitObject(fruits));
+
 // { genus: 'Citrullus', name: 'Watermelon', id: 25, family: 'Cucurbitaceae',
 // order: 'Cucurbitales', nutritions: { carbohydrates: 8, protein: 0.6, fat:
 // 0.2, calories: 30, sugar: 6 } }
@@ -25,14 +26,15 @@ console.log(lastFruitObject(fruits));
 
 function lastFruitObject(fruits) {
     // Your code here
+    return fruits[fruits.length - 1];
 }
 
-
+//console.log(lastFruitObject(fruits));
 /* 03. `indexFruitObject()` - Return one object at the given index from the
 fruits array. The function should have two parameters, the array and the
 index of the item.
 
-console.log(indexFruitObject(17, fruits));
+
 // { genus: 'Mangifera', name: 'Mango', id: 27, family: 'Anacardiaceae',
 // order: 'Sapindales', nutritions: { carbohydrates: 15, protein: 0.82, fat:
 // 0.38, calories: 60, sugar: 13.7 } };
@@ -40,12 +42,13 @@ console.log(indexFruitObject(17, fruits));
 
 function indexFruitObject(index, fruits) {
     // Your code here
+    return fruits[index];
 }
 
-
+//console.log(indexFruitObject(17, fruits));
 /* 04. `fruitNames()` - Return a list of all of the fruit names
 
-console.log(fruitNames(fruits));
+
 // [ 'Apple', 'Apricot', 'Banana', 'Blackberry', 'Blueberry', 'Cherry', 'Durian',
     'Fig', 'Gooseberry', 'Grapes', 'GreenApple', 'Guava', 'Kiwi', 'Lemon',
     'Lime', 'Lingonberry', 'Lychee', 'Mango', 'Melon', 'Orange', 'Papaya',
@@ -55,20 +58,27 @@ console.log(fruitNames(fruits));
 
 function fruitNames(fruits) {
     // Your code here
+    return fruits.map(fruit => fruit.name);
 }
+//console.log(fruitNames(fruits));
 
 /* 05. `getFruitKeys()` - Return a list of all of the keys for each fruit record.
 NOTE: Call a function you previously wrote as a helper function.
 
-// console.log(getFruitKeys(fruits));
-// // [ 'genus', 'name', 'id', 'family', 'order', 'nutritions' ];
+//
+// [ 'genus', 'name', 'id', 'family', 'order', 'nutritions' ];
 */
 
 function getFruitKeys(fruits) {
     // Your code here
+    const keys = [];
+    for (i = 0; i < fruits.length; i++) {
+        keys.push(Object.keys(fruits[i]));
+    }
+    console.log(keys);
 }
 
-
+console.log(getFruitKeys(fruits));
 /* 06. `getNutritionsKeys()` - Return a list of all of the keys within each
 "nutritions" object.
 NOTE: Can you use a different approach than what you used in the previous
